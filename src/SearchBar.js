f0c4f5ab050e22c90f50cd55e7e0b14c9b0d26fc
alexrem1 <remidy@live.co.uk>
2021-06-06 23:12:40 +0100

    Controlled components

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
 function SearchBar(props) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [type, setType] = useState("");
+  const [brand, setBrand] = useState("");
+
   return (
     <div>
       <h2>Search for an Item</h2>
       <form>
         <label for="name-field">Name: </label>
-        <input id="name-field" type="text"></input>
+        <input id="name-field" type="text" value={name}></input>
         <label for="price-field">Max Price: </label>
-        <input id="price-field" type="number"></input>
+        <input id="price-field" type="number" value={price}></input>
         <label for="type-field">Type: </label>
-        <input id="type-field" type="text"></input>
+        <input id="type-field" type="text" value={type}></input>
         <label for="brand-field">Brand: </label>
-        <input id="brand-field" type="text"></input>
+        <input id="brand-field" type="text" value={brand}></input>
         <button type="button">Search</button>
       </form>
     </div>
