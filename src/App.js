326cd3d53dd183a7c9f92df046c3e7d3a2992590
alexrem1 <remidy@live.co.uk>
2021-06-08 19:43:52 +0100

    Displaying Data Dynamically using .map() to Render Data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
 import AddItem from "./AddItem";
+import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
   const [filters, setFilters] = useState({});
@@ -13,6 +14,7 @@ function App() {
 
   const addItemToData = (item) => {
     let items = data["items"];
+    item.id = items.length;
     items.push(item);
     setData({ items: items });
     console.log(data);
@@ -21,6 +23,7 @@ function App() {
   return (
     <div className="App">
       <SearchBar updateSearchParams={updateFilters} />
+      <ItemsDisplay items={data["items"]} />
       <AddItem addItem={addItemToData} />
     </div>
   );
