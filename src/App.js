a6c6164b41f0a55ea8e4c75fe4b37aec7af32efa
alexrem1 <remidy@live.co.uk>
2021-07-08 13:38:44 +0100

    Adding items to the db

--- src/App.js
+++ src/App.js
@@ -20,15 +20,19 @@ function App() {
 
   const addItemToData = (item) => {
     let items = data["items"];
-    item.id = items.length;
     const requestOptions = {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
     };
-    fetch("http://localhost:3000/items", requestOptions);
-
-    items.push(item);
-    setData({ items: items });
-    console.log(data);
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      });
   };
 
   const filterData = (data) => {
